9ca6e3ae1f1d0d4953a8e4809cd0211006b169be
Eduardo Nóbrega <eduardo.nobrrega@gmail.com>
2025-05-05 19:20:14 -0300

    add webpack

---
+++ petshop/src/main.js
@@ -0,0 +1,11 @@
+"use strict"
+
+// CSS
+import "./styles/global.css"
+import "./styles/header.css"
+import "./styles/input.css"
+import "./styles/schedules.css"
+import "./styles/button.css"
+import "./styles/form.css"
+
+console.log("Hello Webpack!")
