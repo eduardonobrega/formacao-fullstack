e477d7da6e3eccd7b56f5054d4042dfbc3474346
eduardonobrega <eduardo.nobrrega@gmail.com>
2025-05-08 10:39:25 -0300

    fetch and show schedules

--- petshop/src/modules/form/date-change.js
+++ petshop/src/modules/form/date-change.js
@@ -1,5 +1,12 @@
+import { schedulesDay } from "../schedules/load.js"
 import { hoursLoad } from "./hours-load.js"
 
-const selectedDate = document.getElementById("dateForm")
+const dateForm = document.getElementById("dateForm")
+const date = document.getElementById("date")
 
-dateForm.addEventListener("change", () => hoursLoad({ date: selectedDate.value }))
+dateForm.addEventListener("change", () => hoursLoad({ date: dateForm.value }))
+
+date.addEventListener("change", () => {
+    schedulesDay({ date: date.value })
+    dateForm.value = date.value
+})
--- petshop/src/modules/schedules/load.js
+++ petshop/src/modules/schedules/load.js
@@ -1,10 +1,19 @@
-import { hoursLoad } from "../form/hours-load"
+import { fetchSchedules } from "../../services/fetch-schedules.js"
+import { hoursLoad } from "../form/hours-load.js"
+import { showSchedules } from "./show.js"
 
 const selectedDate = document.getElementById("date")
 
 // Buscar os agendamentos do dia selecionado e carregar as horas disponíveis
-export function schedulesDay() {
+export async function schedulesDay() {
     const date = selectedDate.value
-    
+
+    // Buscando os agendamentos na API
+    const dailySchedules = await fetchSchedules({ date })
+
+    // Carregando as horas disponíveis
     hoursLoad({ date })
+
+    // Renderizando os agendamentos do dia selecionado
+    showSchedules(dailySchedules)
 }
